3d82163181fb778df4dca70172f606640259a53c
Cullen Smith <cullensmi@gmail.com>
2020-10-01 10:37:48 -0400

    final commit

--- booklist/src/components/BookList.js
+++ booklist/src/components/BookList.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import BookDetails from './BookDetails';
+import { BookContext } from '../contexts/BookContext';
 
-export default function BookList() {
-    return (
-        <div>
-            
+
+const BookList = () => {
+    const { books } = useContext(BookContext);
+    return books.length ? ( 
+        <div className="book-list">
+            <ul>
+                { books.map(book => {
+                    return ( <BookDetails book={book} key={book.id} />);
+                })}
+
+            </ul>
         </div>
+
+    ) : (
+        <div className="empty">No books to Read. Hello free time :) </div>
     )
 }
+
+export default BookList; 
